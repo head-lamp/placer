ee60e6ccb2480cf98b7f900c457b6abb6aed4496
Weston Boldt <westonboldt@Westons-MacBook-Air.local>
2020-04-27 23:23:46 -0500

    guess who learned how to compile c++

--- Makefile
+++ Makefile
@@ -1,8 +1,10 @@
 objs = main.cpp
 cc = g++
 compiler_flags = -w -v
-linker_flags = -I include -L lib -l SDL2-2.0.0 -lSDL2_image
+# linker_flags = -I include -L lib -l SDL2-2.0.0 -lSDL2_image
+linker_flags = -I /usr/local/include -L /usr/local/lib -l SDL2-2.0.0 -lSDL2_image
 obj_name = game
 
 all : $(objs)
 	$(cc) $(objs) $(compiler_flags) $(linker_flags) -o $(obj_name)
+	./game
--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@
 int init(void);
 int load_media(void);
 void close(void);
-SDL_Surface* loadSurface(const char*);
+SDL_Surface* load_surface(const char*);
+SDL_Texture* load_texture(const char*);
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
@@ -14,6 +15,8 @@ SDL_Window* window = NULL;
 SDL_Surface* screenSurface = NULL;
 SDL_Surface* helloWorld = NULL;
 SDL_Surface* currentSurface = NULL;
+SDL_Renderer* renderer = NULL;
+SDL_Texture* texture = NULL;
 
 int init(void) {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
@@ -27,23 +30,59 @@ int init(void) {
         printf("window couldn't be created SDL err: %s\n", SDL_GetError());
         return 0;
     }
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == NULL) {
+        printf("could not create render SDL err: %s\n", SDL_GetError());
+        return 0;
+    }
 
-    screenSurface = SDL_GetWindowSurface(window);
-    return 1;
-}
-
-int load_media(void) {
-    helloWorld = loadSurface("assets/stretch.bmp");
-    if (helloWorld == NULL) {
+    SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+    int imgflags = IMG_INIT_PNG;
+    if (!(IMG_Init(imgflags) & imgflags)) {
+        printf("couldn't init sdl_image SDL err: %s\n", IMG_GetError());
         return 0;
     }
 
     return 1;
 }
 
-SDL_Surface* loadSurface(const char* path) {
+int load_media(void) {
+    //helloWorld = load_surface("assets/loaded.png");
+    texture = load_texture("assets/loaded.png");
+    if (texture == NULL) {
+        printf("failed to load texture image\n");
+        return 0;
+    }
+    /*
+    if (helloWorld == NULL) {
+        return 0;
+    }
+
+    */
+    return 1;
+}
+
+SDL_Texture* load_texture(const char* path) {
+    SDL_Texture *new_texture = NULL;
+    SDL_Surface *loaded_surface = IMG_Load(path);
+    if (loaded_surface == NULL) {
+        printf("unable to load img %s SDL_image err: %s\n",
+            path, IMG_GetError());
+    }
+    new_texture = SDL_CreateTextureFromSurface(renderer, loaded_surface);
+    if (new_texture == NULL) {
+        printf("unable to create texture from %s SDL err: %s\n",
+            path, SDL_GetError());
+    }
+    SDL_FreeSurface(loaded_surface);
+
+    return new_texture;
+}
+
+SDL_Surface* load_surface(const char* path) {
     SDL_Surface* optimizedSurface = NULL;
-    SDL_Surface* loadedSurface = SDL_LoadBMP(path);
+    // SDL_Surface* loadedSurface = SDL_LoadBMP(path);
+    SDL_Surface* loadedSurface = IMG_Load(path);
     if (loadedSurface == NULL) {
         printf("unable to load image %s SDL err: %s\n", path, SDL_GetError());
     }
@@ -57,9 +96,14 @@ SDL_Surface* loadSurface(const char* path) {
 
 void close(void) {
     SDL_FreeSurface(helloWorld);
-    helloWorld = NULL;
+    SDL_DestroyTexture(texture);
+    texture = NULL;
+    // helloWorld = NULL;
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     window = NULL;
+    renderer= NULL;
+    IMG_Quit();
     SDL_Quit();
 }
 
@@ -93,12 +137,11 @@ int main(int argc, char* args[]) {
             else if (e.type == SDL_KEYDOWN) {
             }
         }
-        stretchRect.x = 0;
-        stretchRect.y = 0;
-        stretchRect.w = SCREEN_WIDTH;
-        stretchRect.h = SCREEN_HEIGHT;
-        SDL_BlitScaled(currentSurface, NULL, screenSurface, &stretchRect);
-        SDL_UpdateWindowSurface(window);
+        SDL_RenderClear(renderer);
+
+        SDL_RenderCopy(renderer, texture, NULL, NULL);
+
+        SDL_RenderPresent(renderer);
     }
 
     close();
